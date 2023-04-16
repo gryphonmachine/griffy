9ecf48bd24f1fe2152f34739d4e2af63852bbbe4
heybereket <semagnbereket@gmail.com>
2023-04-16 13:34:02 -0400

    add playoffs data for /comp

--- src/commands/Event.ts
+++ src/commands/Event.ts
@@ -4,11 +4,11 @@ import {
   ApplicationCommandType,
   ApplicationCommandOptionType,
   EmbedBuilder,
-  codeBlock,
 } from "discord.js";
 import { Command } from "../lib/Comamnd";
 import fetch from "node-fetch";
 import { eventChoices, yearChoices } from "../lib/choices/events";
+import { isEqual } from "lodash";
 
 export const Event: Command = {
   name: "comp",
@@ -35,71 +35,159 @@ export const Event: Command = {
     const year = interaction.options.get("year")?.value;
 
     const nonDistrictEvents = ["worlds", "provincials"];
+    let invalidEvent = false;
+    const qualMatches = await fetch(
+      `https://frc6070.ca/api/${year}/${event}`
+    ).then(async (res) => {
+      if (res.status === 200) {
+        invalidEvent = false;
+        return res.json();
+      } else {
+        return (invalidEvent = true);
+      }
+    });
 
-    await fetch(`https://frc6070.ca/api/${year}/${event}`)
-      .then(async (res) => {
-        const data = await res.json();
+    const playoffMatches = await fetch(
+      `https://frc6070.ca/api/${year}/${event}?type=Playoff`
+    ).then((res) => {
+      if (res.status === 200) {
+        invalidEvent = false;
+        return res.json();
+      } else {
+        return (invalidEvent = true);
+      }
+    });
 
-        const embed = new EmbedBuilder()
-          .setColor("#fbbb04")
-          .setTitle(
-            `${year} ${
-              String(event).charAt(0).toUpperCase() + String(event).slice(1)
-            } ${
-              nonDistrictEvents.some((v) => String(event).includes(v))
-                ? "Event"
-                : "District Event"
-            }`
-          )
-          .setTimestamp();
-
-        data.forEach((match: any) => {
-          const team6070 = match.teams.filter(
-            (team: any) => team.teamNumber == "6070"
-          );
-          const alliance = team6070[0].station.replace(/[0-9]/g, "");
-
-          const didWeWin = () => {
-            if (
-              match.scoreRedFinal > match.scoreBlueFinal &&
-              alliance === "Red"
-            ) {
-              return true;
-            } else if (
-              match.scoreBlueFinal > match.scoreRedFinal &&
-              alliance === "Blue"
-            ) {
-              return true;
-            } else {
-              return false;
-            }
-          };
-
-          const firstAllianceFilter = match.teams.filter((team: any) => {
-            return team.station.includes(
-              team6070[0].station.replace(/[0-9]/g, "")
-            );
-          });
-
-          const secondAllianceFilter = firstAllianceFilter.map((team: any) => {
-            return team.teamNumber;
-          });
-
-          embed.addFields({
-            name: `${didWeWin() ? "🏆" : ""} ${match.description}`,
-            value: `[${alliance}] **Alliance:** ${secondAllianceFilter
-              .toString()
-              .replace(/,/g, ", ")}`,
-            inline: true,
-          });
-        });
-
-        return await interaction.reply({
-          embeds: [embed],
-        });
-      })
-      .catch(async (error) => {
-        return await interaction.reply(codeBlock("ts", error));
+    if (invalidEvent) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor("#fbbb04")
+            .setTitle("ERROR")
+            .setDescription("Uh oh! Looks like that event **doesn't exist.**")
+            .setTimestamp(),
+        ],
       });
+    }
+
+    const didWeWin = (match: any, alliance: any) => {
+      if (match.scoreRedFinal > match.scoreBlueFinal && alliance === "Red") {
+        return true;
+      } else if (
+        match.scoreBlueFinal > match.scoreRedFinal &&
+        alliance === "Blue"
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    const qualEmbed = new EmbedBuilder()
+      .setColor("#fbbb04")
+      .setTitle(
+        `${year} ${
+          String(event).charAt(0).toUpperCase() + String(event).slice(1)
+        } ${
+          nonDistrictEvents.some((v) => String(event).includes(v))
+            ? "Event"
+            : "District Event"
+        } (Qualifications)`
+      )
+      .setTimestamp();
+
+    const playoffEmbed = new EmbedBuilder()
+      .setColor("#fbbb04")
+      .setTitle(
+        `${year} ${
+          String(event).charAt(0).toUpperCase() + String(event).slice(1)
+        } ${
+          nonDistrictEvents.some((v) => String(event).includes(v))
+            ? "Event"
+            : "District Event"
+        } (Playoffs)`
+      )
+      .setTimestamp();
+
+    if (isEqual(qualMatches, playoffMatches)) {
+      qualMatches.forEach(async (match: any) => {
+        const team6070 = match.teams.filter(
+          (team: any) => team.teamNumber == "6070"
+        );
+        const alliance = team6070[0].station.replace(/[0-9]/g, "");
+
+        const firstAllianceFilter = match.teams.filter((team: any) => {
+          return team.station.includes(
+            team6070[0].station.replace(/[0-9]/g, "")
+          );
+        });
+
+        const secondAllianceFilter = firstAllianceFilter.map((team: any) => {
+          return team.teamNumber;
+        });
+
+        qualEmbed.addFields({
+          name: `${didWeWin(match, alliance) ? "🏆" : ""} ${match.description}`,
+          value: `[${alliance}] **Alliance:** ${secondAllianceFilter
+            .toString()
+            .replace(/,/g, ", ")}`,
+          inline: true,
+        });
+      });
+
+      return interaction.reply({ embeds: [qualEmbed] });
+    } else {
+      qualMatches.forEach(async (match: any) => {
+        const team6070 = match.teams.filter(
+          (team: any) => team.teamNumber == "6070"
+        );
+        const alliance = team6070[0].station.replace(/[0-9]/g, "");
+
+        const firstAllianceFilter = match.teams.filter((team: any) => {
+          return team.station.includes(
+            team6070[0].station.replace(/[0-9]/g, "")
+          );
+        });
+
+        const secondAllianceFilter = firstAllianceFilter.map((team: any) => {
+          return team.teamNumber;
+        });
+
+        qualEmbed.addFields({
+          name: `${didWeWin(match, alliance) ? "🏆" : ""} ${match.description}`,
+          value: `[${alliance}] **Alliance:** ${secondAllianceFilter
+            .toString()
+            .replace(/,/g, ", ")}`,
+          inline: true,
+        });
+      });
+
+      playoffMatches.forEach(async (match: any) => {
+        const team6070 = match.teams.filter(
+          (team: any) => team.teamNumber == "6070"
+        );
+        const alliance = team6070[0].station.replace(/[0-9]/g, "");
+
+        const firstAllianceFilter = match.teams.filter((team: any) => {
+          return team.station.includes(
+            team6070[0].station.replace(/[0-9]/g, "")
+          );
+        });
+
+        const secondAllianceFilter = firstAllianceFilter.map((team: any) => {
+          return team.teamNumber;
+        });
+
+        playoffEmbed.addFields({
+          name: `${didWeWin(match, alliance) ? "🏆" : ""} ${match.description}`,
+          value: `[${alliance}] **Alliance:** ${secondAllianceFilter
+            .toString()
+            .replace(/,/g, ", ")}`,
+          inline: true,
+        });
+      });
+
+      return await interaction.reply({ embeds: [qualEmbed, playoffEmbed] });
+    }
   },
 };
